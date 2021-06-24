26497a81907f94958595590e5e480c90815344d2
박성준 <seong_jun@jun.local>
2021-06-24 18:27:03 +0900

    westagram init-setting

--- students/seongjun/westagram/settings.py
+++ students/seongjun/westagram/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'user'
 ]
 
 MIDDLEWARE = [
@@ -115,6 +114,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REMOVE_APPEND_SLASH_WARNING
+APPEND_SLASH = False
 
 # CORS
 CORS_ORIGIN_ALLOW_ALL = True
