92fd8e692e070b79d82be1b5f509aba54588503a
서정민 <sayho2021@gmail.com>
2021-06-25 01:25:30 +0900

    Remove #comments and revise password field

--- students/jungmin/user/models.py
+++ students/jungmin/user/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model):
-    email    = models.EmailField(max_length=200) #attribute 확인
-    password = models.CharField(max_length=20)
+    email    = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
     phone    = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
 
