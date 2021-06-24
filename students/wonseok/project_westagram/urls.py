d33e5c81ae8ae4731076426ba4e2d89eba175f3f
wonseok2877 <wonseok2877@gmail.com>
2021-06-24 16:50:26 +0900

    user 폴더 저장

--- students/wonseok/project_westagram/urls.py
+++ students/wonseok/project_westagram/urls.py
@@ -1,5 +1,3 @@
-# from django.urls import path, include
+from django.urls import path, include
 
-urlpatterns = [
-    # path("user", include("user.urls"))
-]
+urlpatterns = [path("user", include("user.urls"))]
