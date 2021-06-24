b559ed9a2872a115ee728068ecd82a8e52dec66f
choi myungjun <junlegend82@gmail.com>
2021-06-25 00:55:22 +0900

    Add: models.py

--- students/myungjun/user/models.py
+++ students/myungjun/user/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-
 class User(models.Model):
-    name     = models.CharField(unique=True, max_length=30)
-    email    = models.EmailField(unique=True)
-    password = models.CharField(max_length=30)
-    phone    = models.IntegerField(unique=True, max_length=11)
-    nickname = models.CharField(max_length=30)
+    name     = models.CharField(max_length=50, unique=True)
+    email    = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=200)
+    phone    = models.IntegerField(max_length=30, unique=True)
+    nickname = models.CharField(max_length=50)
 
     class Meta:
         db_table = "users"
