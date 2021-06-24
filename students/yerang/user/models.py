271b90a69b7f0f261772d2485ec3af1df6c24f1b
yerang <grondin0425@gmail.com>
2021-06-24 20:17:36 +0900

    Add: user/models.py

---
+++ students/yerang/user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model): 
+    email        = models.EmailField(max_length=200, unique=True)
+    password     = models.CharField(max_length=45)
+    phone_number = models.CharField(max_length=45, unique=True)
+    nickname     = models.CharField(max_length=45, unique=True)
+
+    class Meta:
+        db_table = 'users'
