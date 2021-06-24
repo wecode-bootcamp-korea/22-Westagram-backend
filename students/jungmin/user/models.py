fb72cb4bc7e792302fed4b22caa3df4a9b379add
서정민 <sayho2021@gmail.com>
2021-06-24 20:11:06 +0900

    Add: indents

--- students/jungmin/user/models.py
+++ students/jungmin/user/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    email = models.EmailField(max_length=200) #attribute 확인
+    email    = models.EmailField(max_length=200) #attribute 확인
     password = models.CharField(max_length=20)
-    phone = models.CharField(max_length=20)
+    phone    = models.CharField(max_length=20)
     nickname = models.CharField(max_length=20)
 
     class Meta:
