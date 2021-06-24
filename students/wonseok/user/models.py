e758facdd11b6ab0b182764c7ab5b6549b30fa6e
wonseok2877 <wonseok2877@gmail.com>
2021-06-24 21:36:19 +0900

    Add: user model

---
+++ students/wonseok/user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class User(models.Model):
+    email = models.EmailField(
+        max_length=100,
+        unique=True,
+    )
+    password = models.CharField(
+        max_length=30,
+    )
+    nick_name = models.CharField(max_length=100, unique=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.nick_name
