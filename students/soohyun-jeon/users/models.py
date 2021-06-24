fac0b372408c6cf687f464b2a05d50a8da9bea66
JeonSoohyun27 <83414688+JeonSoohyun27@users.noreply.github.com>
2021-06-25 00:33:33 +0900

    Update models.py

--- students/soohyun-jeon/users/models.py
+++ students/soohyun-jeon/users/models.py
@@ -2,11 +2,11 @@
 from django.db                    import models
 
 class User(models.Model):
-    user_phonenumber = models.CharField(max_length=30)
-    user_email       = models.EmailField(max_length=50)
-    user_name        = models.CharField(max_length=30)
-    user_nickname    = models.CharField(max_length=30)
-    user_password    = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=30)
+    email        = models.EmailField(max_length=50)
+    name         = models.CharField(max_length=30)
+    nickname     = models.CharField(max_length=30)
+    password     = models.CharField(max_length=200)
 
     class Meta:
         db_table = 'users'
