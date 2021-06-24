213f0fef73463cc371baa227ec874500933d0624
anjaekk <anjaekk@gmail.com>
2021-06-25 00:29:32 +0900

    Modify: User Model 리뷰 반영

--- students/jaegyeong-an/user/models.py
+++ students/jaegyeong-an/user/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class User(models.Model):
     email        = models.EmailField(max_length=200, unique=True)
-    phone_number = models.CharField(max_length=100, unique=True, blank=True)
-    name         = models.CharField(max_length=100, blank=True)
-    nickname     = models.CharField(max_length=100, unique=True, blank=True)
+    phone_number = models.CharField(max_length=100, unique=True, null=True)
+    name         = models.CharField(max_length=100)
+    nickname     = models.CharField(max_length=100, unique=True, null=True)
     password     = models.CharField(max_length=200)
-    created_at   = models.DateField(auto_now_add=True)
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
