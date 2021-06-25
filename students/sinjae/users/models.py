16e322236789eedaacd64c4f8f27683c533a491e
Jay9393 <tlswo1313@gmail.com>
2021-06-25 16:26:01 +0900

    Add: Users Models

--- students/sinjae/users/models.py
+++ students/sinjae/users/models.py
@@ -1,4 +1,4 @@
-from django.db                  import models
+ from django.db                  import models
 from django.db.models.deletion  import CASCADE
 
 class User(models.Model):
@@ -10,16 +10,17 @@ class User(models.Model):
         ('prefer not to say', 'Prefer Not To Say')
     )
 
-    nick_name       = models.CharField(max_length=20) 
-    name            = models.CharField(max_length=40) 
-    password        = models.CharField(max_length=200)
-    website         = models.CharField(max_length=100, null=True)
-    bio             = models.CharField(max_length=100, null=True)
-    email           = models.CharField(max_length=100)
-    phone_number    = models.CharField(max_length=30) 
-    gender          = models.CharField('Gender', max_length=20, choices= gender_choice)
-    birth_date      = models.DateField()
-    created_at     = models.DateTimeField(auto_now=True, auto_now_add=True)
-    
+    nick_name    = models.CharField(max_length=20)
+    name         = models.CharField(max_length=40)
+    password     = models.CharField(max_length=200)
+    websit       = models.CharField(max_length=100, null=True)
+    bio          = models.CharField(max_length=100, null=True)
+    email        = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=30)
+    gender       = models.CharField('Gender', max_length=20, choices= gender_choice)
+    birth_date   = models.DateField()
+    created_at   = models.DateTimeField(auto_now=True)
+    updated_at   = models.DateTimeField(auto_now_add=True)
+
     class Meta:
-        db_table    = 'users'
+        db_table = 'users'
