9d317d28cfde847da8225738b9db0c51d900853f
heesu-ahn <anhesu4@gmail.com>
2021-06-25 14:08:44 +0900

    model changed

--- students/heesu/users/models.py
+++ students/heesu/users/models.py
@@ -1,13 +1,11 @@
 from django.db  import models
 
 class User(models.Model) :
-    # Not Nullable Field (Order by Sinup Form's input_Seq)
-    phone_number    = models.CharField(unique=True,max_length=100,null=False)
-    email           = models.EmailField(unique=True,max_length=100,null=False)
-    full_name       = models.CharField(max_length=50,null=False)
-    password        = models.CharField(max_length=200,null=False)
-    nick_name       = models.CharField(max_length=50,null=False)
-    # Nullable Field (Do not use when the First SignUp)
+    phone_number    = models.CharField(unique=True,max_length=30)
+    email           = models.EmailField(unique=True,max_length=30)
+    full_name       = models.CharField(max_length=45)
+    password        = models.CharField(max_length=200)
+    nick_name       = models.CharField(max_length=30)
     created_at      = models.DateTimeField(auto_now_add=True)
     updated_at      = models.DateTimeField(auto_now=True)
     login_at        = models.DateTimeField()
