849599be381279b9ef4c4be5bb40f32474850b04
anjaekk <anjaekk@gmail.com>
2021-06-25 09:52:50 +0900

    Modify: User Model nickname t속성수정

--- students/jaegyeong-an/user/models.py
+++ students/jaegyeong-an/user/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     email        = models.EmailField(max_length=200, unique=True)
     phone_number = models.CharField(max_length=100, unique=True, null=True)
     name         = models.CharField(max_length=100)
-    nickname     = models.CharField(max_length=100, unique=True, null=True)
+    nickname     = models.CharField(max_length=100, unique=True)
     password     = models.CharField(max_length=200)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
