a4b2f7997b221e92b76ec06868c948ca49d3b11d
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-25 14:12:16 +0900

    update

--- students/jiwoo/users/models.py
+++ students/jiwoo/users/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class User(models.Model): 
     email        = models.EmailField()
-    password     = models.CharField(max_length=50)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=20)
     name         = models.CharField(max_length=30)
+    
     class Meta: 
         db_table = 'users'
--- students/jiwoo/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from users.views import SignupsView
-
-urlpatterns = [
-    path('', SignupsView.as_view())
-]
