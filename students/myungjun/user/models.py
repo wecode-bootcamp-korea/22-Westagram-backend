2eed817c267ac453567df46f64136b41ec9dc7a5
choi myungjun <junlegend82@gmail.com>
2021-06-25 11:00:41 +0900

    Add: phone type change

--- students/myungjun/user/models.py
+++ students/myungjun/user/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     name     = models.CharField(max_length=50, unique=True)
     email    = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=200)
-    phone    = models.IntegerField(max_length=30, unique=True)
+    phone    = models.CharField(max_length=30, unique=True)
     nickname = models.CharField(max_length=50)
 
     class Meta:
