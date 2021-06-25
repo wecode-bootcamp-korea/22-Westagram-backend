27207da405aa60aec87fe3f4bec3172c3836cd89
Dongmyeong Lee <dom9316@gmail.com>
2021-06-25 14:36:15 +0900

    Fix: Field of phonenumber & max_length of password

--- students/dongmyeong/users/models.py
+++ students/dongmyeong/users/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class User(models.Model):
     email    = models.CharField(max_length=320, unique=True)
-    password = models.CharField(max_length=100)
-    phone    = models.IntegerField(unique=True)
+    password = models.CharField(max_length=200)
+    phone    = models.CharField(max_length=20, unique=True)
     nickname = models.CharField(max_length=50, unique=True)
 
     class Meta:
