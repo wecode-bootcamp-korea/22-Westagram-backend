2c89d693daca0c7c56a91cb137644ab41be3deee
anjaekk <anjaekk@gmail.com>
2021-06-25 18:57:02 +0900

    Modify: Review 반영

--- students/jaegyeong-an/user/views.py
+++ students/jaegyeong-an/user/views.py
@@ -9,28 +9,26 @@ from user.models  import User
 
 class SignUpView(View):
     def post(self, request):
+        REGEX_MAIL = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
         try:
             data = json.loads(request.body)
             email = data['email']
-            email_pattern = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-            if email_pattern.match(email) == None:
+
+            if REGEX_MAIL.match(email) == None:
                 return JsonResponse({'error':'INVALID_EMAIL'}, status=400)
             if len(data['password']) < 8:
                 return JsonResponse({'error':'INVALID_PASSWORD'}, status=400)
-            else:
-                User.objects.create(
-                email = data['email'],
-                phone_number = re.sub('-', '', data['phone_number']),
-                name = data['name'],
-                nickname = data['nickname'],
-                password = data['password']
-                )
+                
+            User.objects.create(
+            email        = data['email'],
+            phone_number = data['phone_number'],
+            name         = data['name'],
+            nickname     = data['nickname'],
+            password     = data['password']
+            )
             return JsonResponse({'message':'SUCCESS'}, status=201)
 
         except KeyError:
             return JsonResponse({'error':'KEY_ERROR'}, status=400)
         except IntegrityError:
             return JsonResponse({'error':'DUPLICATE_ENTRY'}, status=400)
-
-
-
