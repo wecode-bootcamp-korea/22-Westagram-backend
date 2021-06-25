439573072f6b3dc8a2199fd4cb24c0178d18343f
박성준 <seong_jun@jun.local>
2021-06-25 10:09:29 +0900

    westagram models

--- students/seongjun/user/models.py
+++ students/seongjun/user/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Account(models.Model):
     email           = models.EmailField(max_length=100)
-    password        = models.CharField(max_length=45)
+    password        = models.CharField(max_length=200)
     nick_name       = models.CharField(max_length=45)
-    phone_number    = models.CharField(max_length=100)
-    register_dttm   = models.DateField(auto_now_add=True)
-    update_dttm     = models.DateField(auto_now=True)
+    phone_number    =  models.CharField(max_length=45)
+    created_at      = models.DateField(auto_now_add=True)
+    updated_at      = models.DateField(auto_now=True)
 
     class Meta:
         db_table = 'accounts'
