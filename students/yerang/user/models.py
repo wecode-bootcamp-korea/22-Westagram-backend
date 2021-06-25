92c65f2af77c268451b769ece73de696128ec6ed
yerang <grondin0425@gmail.com>
2021-06-25 11:38:16 +0900

    Mission2: models.py 패스워드, 이메일필드 max_length 수정

--- students/yerang/user/models.py
+++ students/yerang/user/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class User(models.Model): 
-    email        = models.EmailField(max_length=200, unique=True)
-    password     = models.CharField(max_length=45)
+    email        = models.EmailField(max_length=45, unique=True)
+    password     = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=45, unique=True)
     nickname     = models.CharField(max_length=45, unique=True)
 
