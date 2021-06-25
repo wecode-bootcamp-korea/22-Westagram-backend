cb0dacc306c3ffd3c4e04f4e18bbe8e8d2ef9df1
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-06-25 12:54:03 +0900

    Add: User Model 수정2

--- students/hanjoon/users/models.py
+++ students/hanjoon/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     password     = models.CharField(max_length=200)
     email        = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
-    nickname    = models.CharField(max_length=50, unique=True)
+    nickname     = models.CharField(max_length=50, unique=True)
     
     class Meta:
         db_table = 'users'
--- students/hanjoon/users/urls.py
+++ students/hanjoon/users/urls.py
@@ -1,6 +1,4 @@
-from django.urls import path
 from users.views import StargramView
 
 urlpatterns = [
-	path('/user', StargramView.as_view()),
 	]
