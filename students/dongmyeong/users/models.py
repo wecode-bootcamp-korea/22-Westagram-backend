da595d5fe9f6a651d35982d0ce49558d78241e91
Dongmyeong Lee <dom9316@gmail.com>
2021-06-25 14:14:16 +0900

    Add: User modeling

--- students/dongmyeong/users/models.py
+++ students/dongmyeong/users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-	email = models.CharField(max_length=320, unique=True)
-	password = models.CharField(max_length=100)
-	phone = models.IntegerField(unique=True)
-	nickname = models.CharField(max_length=50, unique=True)
+    email    = models.CharField(max_length=320, unique=True)
+    password = models.CharField(max_length=100)
+    phone    = models.IntegerField(unique=True)
+    nickname = models.CharField(max_length=50, unique=True)
 
-	class Meta:
-		db_table = 'users'
+    class Meta:
+        db_table = 'users'
 
-	def __str__(self):
-		return ', '.join([self.id, self.email, self.password, self.phone, self.nickname])
+    def __str__(self):
+        return ', '.join([self.id, self.email, self.password, self.phone, self.nickname])
