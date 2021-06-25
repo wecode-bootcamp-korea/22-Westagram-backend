3f1ccadd9102c39a32bdf2ee78b4ae48bc911842
heesu-ahn <anhesu4@gmail.com>
2021-06-25 13:22:32 +0900

    conversation and models changed

--- students/heesu/users/models.py
+++ students/heesu/users/models.py
@@ -1,16 +1,15 @@
-from datetime   import datetime
 from django.db  import models
 
-class USER(models.Model) :
+class User(models.Model) :
     # Not Nullable Field (Order by Sinup Form's input_Seq)
-    phone_nmber     = models.CharField(unique=True,max_length=100,null=False)   # Instagram's Phone Number(Unique)
-    email           = models.EmailField(unique=True,max_length=100,null=False)  # Instagram's Email(Unique)
-    full_name       = models.CharField(max_length=50,null=False,default='')     # Instagram's Full Name
-    password        = models.CharField(max_length=200,null=False)               # Instagram's Password
-    nick_name       = models.CharField(max_length=50,null=False,default='')     # Instagram's Username
+    phone_nmber     = models.CharField(unique=True,max_length=100,null=False)
+    email           = models.EmailField(unique=True,max_length=100,null=False)
+    full_name       = models.CharField(max_length=50,null=False)
+    password        = models.CharField(max_length=200,null=False)
+    nick_name       = models.CharField(max_length=50,null=False)
     # Nullable Field (Do not use when the First SignUp)
-    created_at      = models.DateTimeField(default=datetime.now)                # insert current_time when first input
-    updated_at      = models.DateTimeField(default=datetime.now)                # insert current_time when update user's infomation
-    login_at        = models.DateTimeField(null=True)                           # insert current_time when login sucess
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
+    login_at        = models.DateTimeField()
     class Meta :
         db_table    = 'users'
