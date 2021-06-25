01ed2607ffd44f9dc4c550ecee35d405646c4994
박성준 <seong_jun@jun.psj>
2021-06-25 23:09:32 +0900

    westagram signup

---
+++ students/seongjun/user/validators.py
@@ -0,0 +1,16 @@
+import re
+
+def validate_email(value):
+    email_regex = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+    return re.match(email_regex, value)
+
+def validate_phone_number(value):
+    phone_regex = re.compile(r'^01([0|1]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    if not re.match(phone_regex, value):
+        return 0
+    value = re.sub("-", "", value)
+    return value
+
+def validate_password(value):
+    password_regex = re.compile(r'^.{8,}$')
+    return re.match(password_regex, value)
