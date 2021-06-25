a570505f2b2eb25b1912263a998e987d25f6fb3e
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-06-25 11:40:56 +0900

    Add: User Model 수정

--- students/hanjoon/users/models.py
+++ students/hanjoon/users/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
-    name        = models.CharField(max_length=100, unique=True)
-    password    = models.CharField(max_length=100) 
-    email       = models.EmailField(unique=True)
-    phone_number= models.CharField(max_length=100, unique=True)
-    nick_name   = models.CharField(max_length=100, unique=True)
+    name         = models.CharField(max_length=20)
+    password     = models.CharField(max_length=200)
+    email        = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    nickname    = models.CharField(max_length=50, unique=True)
     
     class Meta:
         db_table = 'users'
