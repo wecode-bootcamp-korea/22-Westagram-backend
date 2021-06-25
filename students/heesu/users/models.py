1bf8efa54a6bd7055bd504245a8647f0af48f6fc
heesu-ahn <anhesu4@gmail.com>
2021-06-25 13:55:41 +0900

    2nd commit

--- students/heesu/users/models.py
+++ students/heesu/users/models.py
@@ -2,7 +2,7 @@ from django.db  import models
 
 class User(models.Model) :
     # Not Nullable Field (Order by Sinup Form's input_Seq)
-    phone_nmber     = models.CharField(unique=True,max_length=100,null=False)
+    phone_number    = models.CharField(unique=True,max_length=100,null=False)
     email           = models.EmailField(unique=True,max_length=100,null=False)
     full_name       = models.CharField(max_length=50,null=False)
     password        = models.CharField(max_length=200,null=False)
