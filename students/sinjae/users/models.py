f2280edf64e4947308c1581b7130dba31a10071f
Jay9393 <tlswo1313@gmail.com>
2021-06-25 14:45:30 +0900

    Add: Westagram  Users Models

--- students/sinjae/users/models.py
+++ students/sinjae/users/models.py
@@ -10,16 +10,16 @@ class User(models.Model):
         ('prefer not to say', 'Prefer Not To Say')
     )
 
-    full_name       = models.CharField(max_length=20) # nick name
-    user_name       = models.CharField(max_length=40) 
+    nick_name       = models.CharField(max_length=20) 
+    name            = models.CharField(max_length=40) 
     password        = models.CharField(max_length=200)
-    user_website    = models.CharField(max_length=100, blank=True)
-    user_bio        = models.CharField(max_length=100, blank=True)
-    email           = models.CharField(max_length=100) # id
+    website         = models.CharField(max_length=100, null=True)
+    bio             = models.CharField(max_length=100, null=True)
+    email           = models.CharField(max_length=100)
     phone_number    = models.CharField(max_length=30) 
     gender          = models.CharField('Gender', max_length=20, choices= gender_choice)
     birth_date      = models.DateField()
-    create_date     = models.DateTimeField()
+    created_at     = models.DateTimeField(auto_now=True, auto_now_add=True)
     
     class Meta:
         db_table    = 'users'
