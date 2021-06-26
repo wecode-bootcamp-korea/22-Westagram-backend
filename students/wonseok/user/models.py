bab72b226a8d97a3b24a88f34fa21690f0cf04ba
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 19:52:38 +0900

    UPDATE : views.py models.py

--- students/wonseok/user/models.py
+++ students/wonseok/user/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
     email        = models.EmailField(max_length=100,unique=True,)
     password     = models.CharField(max_length=30)
-    name         = models.CharField(max_length=50)
     nick_name    = models.CharField(max_length=100)
+    name         = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20, unique=True)
     created_at   = models.DateTimeField(auto_now_add=True)
     updated_at   = models.DateTimeField(auto_now=True)
