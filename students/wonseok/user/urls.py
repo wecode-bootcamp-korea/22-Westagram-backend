e46c670e882317e92cb9057b6deb3e915dc76073
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 15:06:44 +0900

    ADD : views.py login logic

--- students/wonseok/user/urls.py
+++ students/wonseok/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from user.views import SignupView
+from user.views import LoginView, SignupView
 urlpatterns = [
-    path("/signup", SignupView.as_view())
+    path("/signup", SignupView.as_view()),
+    path("/login", LoginView.as_view())
 ]
