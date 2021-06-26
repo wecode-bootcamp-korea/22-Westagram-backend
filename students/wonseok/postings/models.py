24cbbbeb8da7f461ebe8da34e442cf0cfc10b29a
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 22:00:10 +0900

    ADD : views.py models.py like

--- students/wonseok/postings/models.py
+++ students/wonseok/postings/models.py
@@ -3,14 +3,24 @@ from django.db import models
 from user.models import User
 
 class Post(models.Model):
-    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    user        = models.ForeignKey(User, on_delete=models.CASCADE, related_name="own_post")
+    liked_user  = models.ManyToManyField(User,through="PostLikeUser", related_name="liked_post")
     image_url   = models.URLField()
     content     = models.TextField()
     created_at  = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
-
+    
     class Meta():
         db_table = "posts"
+
+class PostLikeUser(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    post        = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    
+    class Meta():
+        db_table = "posts_like_users"
 
 class Comment(models.Model):
     user        = models.ForeignKey(User, on_delete=models.CASCADE)
