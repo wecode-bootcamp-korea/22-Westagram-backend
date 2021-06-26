852014dbf3116cb5181cf86a61c63b61edcec920
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 20:41:00 +0900

    ADD : postings app

--- students/wonseok/postings/views.py
+++ students/wonseok/postings/views.py
@@ -1,60 +1,50 @@
 import json
 
 from django.views import View
-from django.http import JsonResponse
+from django.http.response import JsonResponse
 from django.core.exceptions import ValidationError
-from django.db.utils import DataError, IntegrityError
 from json.decoder import JSONDecodeError
 
 from user.models import User
-from user.validation import validate_email, validate_password
+from postings.models import Post
 
-class SignupView(View):
+class RegisterPostView(View):
     def post(self, request):
         try:
-            data = json.loads(request.body)
-            if not validate_email(data["email"]):
-                raise ValidationError(message="EMAIL_VALIDATION_ERROR")
-            if not validate_password(data["password"]):
-                raise ValidationError(message="PASSWORD_VALIDATION_ERROR")
-            User.objects.create(
-                email        = data["email"],
-                password     = data["password"],
-                nick_name    = data["nick_name"],
-                phone_number = data["phone_number"],
-            )
+            data = json.loads(request.body) 
+            if User.objects.filter(id=data["user_id"]).exists():
+                user = User.objects.get(id= data["user_id"])
+            else:
+                raise ValidationError(message="USER_DOESN'T EXIST")
+            image_url = data["image_url"]
+            content   = data["content"]
+            Post.objects.create(user=user, image_url=image_url, content=content)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-        except ValidationError as error:
-            return JsonResponse({"message": error.args[0]}, status=400)
-        except IntegrityError as error:
-            return JsonResponse({"message": "INTEGRITY_ERROR","content":error.args[1] }, status=400)
-        except DataError:
-            return JsonResponse({"message": "DATA_ERROR"}, status=400)
         except JSONDecodeError:
-            return JsonResponse({"message": "JSON_DECODE_ERROR"}, status=400)
-        except Exception:
-            return JsonResponse({"message": "UNCATCHED_ERROR"}, status=400)
-        return JsonResponse({"message": "SUCCESS"}, status=201)
-
-class LoginView(View):
-    def post(self,request):
-        try:
-            data = json.loads(request.body)
-            if not validate_email(email=data["email"]):
-                raise ValidationError(message="INVALID_USER")
-            if not validate_password(password=data["password"]):
-                raise ValidationError(message="INVALID_USER")
-            if User.objects.filter(email=data["email"]).exists():
-                user = User.objects.get(email=data["email"])
-            else :
-                raise ValidationError(message="INVALID_USER")
-            if not validate_password(password= data["password"]) or user.password != data["password"]:
-                raise ValidationError(message="INVALID_USER")
-        except KeyError:
-            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+            return JsonResponse({"message":"JSON_DECODE_ERROR"}, status=400)
         except ValidationError as error:
             return JsonResponse({"message":error.message}, status=400)
         except Exception as error:
-            return JsonResponse({"message":"NOT_CATCHED_ERROR"}, status=400)
-        return JsonResponse({"message":"success"}, status=200)
+            return JsonResponse({"message":"UNCAUGHT_ERROR"}, status=400)
+        return JsonResponse({"message":"success"}, status=201)
+
+class ShowPostView(View):
+    def get(self, request):
+        posts_queryset = Post.objects.all()
+        all_posts = []
+        for post_object in posts_queryset:
+            user = {
+                "nick_name":post_object.user.nick_name,
+                "email":post_object.user.email,
+                "phone_number":post_object.user.phone_number,
+                "created_at":post_object.user.created_at,
+            }
+            post = {
+                "user":user,
+                "image_url":post_object.image_url,
+                "content":post_object.content,
+                "created_at":post_object.created_at
+            }
+            all_posts.append(post)
+        return JsonResponse({"message":"success", "result":all_posts}, status=200)
