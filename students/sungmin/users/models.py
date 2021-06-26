20741bf1858d00bf16df0a497c331c04ad3b3092
tjdlas2412 <alzm2412@gmail.com>
2021-06-26 14:24:02 +0900

    파일수정함

--- students/sungmin/users/models.py
+++ students/sungmin/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
         email           = models.EmailField(max_length=100, unique=True)
         nick_name       = models.CharField(max_length=50, unique=True)
         password        = models.CharField(max_length=200)
-        #phonenumber     = models.CharField(max_length=50)
+        phonenumber     = models.CharField(max_length=50)
         created_at      = models.DateTimeField(auto_now_add=True)
         updated_at      = models.DateTimeField(auto_now=True)
 
