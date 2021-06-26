9ee01e7f8d88e357d4d07a9707a31752acb6a80d
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 11:30:46 +0900

    ADD : views.py signup logic

---
+++ students/wonseok/user/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from user.views import UserView
+urlpatterns = [
+    path("/signup", UserView.as_view())
+]
--- students/wonseok/user/views.py
+++ students/wonseok/user/views.py
@@ -1,45 +1,47 @@
 import json
+from json.decoder import JSONDecodeError
 import re
 
 from django.http import JsonResponse
 from django.views import View
 from django.core.exceptions import ValidationError
 from django.db.utils import DataError, IntegrityError
+from django.core.validators  import validate_email
 
 from user.models import User
 
-email_regex = "^[^@\s]+@[^@\s\.]+\.[^@\.\s]+$"
 
 
-# Create your views here.
 class UserView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        print(data)
+        # contains @ . and other characters inside them
+        email_regex = re.compile("^[^@\s]+@[^@\s\.]+\.[^@\.\s]+$")
+        # Minimum eight characters, at least one letter, one number and one special character
+        password_regex = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
         try:
-            if not re.match(email_regex, data["email"]):
+            data = json.loads(request.body)
+            if not email_regex.match(data["email"]):
                 raise ValidationError(message="EMAIL_VALIDATION_ERROR")
-            if len(data["password"]) < 8:
+            if not password_regex.match(data["password"]):
                 raise ValidationError(message="PASSWORD_VALIDATION_ERROR")
-            new_user = User.objects.create(
-                email=data["email"],
-                password=data["password"],
-                nick_name=data["nick_name"],
-                phone_number=data["phone_number"],
+            User.objects.create(
+                email        = data["email"],
+                password     = data["password"],
+                nick_name    = data["nick_name"],
+                phone_number = data["phone_number"],
             )
-            print(new_user)
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         except ValidationError as error:
             return JsonResponse({"message": error.args[0]}, status=400)
         except IntegrityError as error:
-            return JsonResponse(
-                {"message": "INTEGRITY_ERROR", "content": error.args[1]}, status=400
-            )
+            return JsonResponse({"message": "INTEGRITY_ERROR","content":error.args[1] }, status=400)
         except DataError:
             return JsonResponse({"message": "DATA_ERROR"}, status=400)
+        except JSONDecodeError:
+            return JsonResponse({"message": "JSON_DECODE_ERROR"}, status=400)
         except Exception:
-            return JsonResponse({"message": "SUCCESS"}, status=201)
+            return JsonResponse({"message": "UNCATCHED_ERROR"}, status=400)
 
         return JsonResponse({"message": "SUCCESS"}, status=201)
 
