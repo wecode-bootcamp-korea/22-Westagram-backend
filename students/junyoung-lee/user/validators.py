5dbb36abc1ce2b340301d114d1e77346d5d13672
Junyoung Lee <ambitiouskyle@gmail.com>
2021-06-26 12:20:34 +0900

    UPDATE: REGEX dict, Convention

--- students/junyoung-lee/user/validators.py
+++ students/junyoung-lee/user/validators.py
@@ -1,17 +1,20 @@
 import re
 
+REGEX = {
+    'email'        : '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+    'password'     : '[A-Za-z0-9@#$%^&+=]{8,}',
+    'name'         : '''^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$|^[가-힣]{2,}''', 
+    'phone_number' : '^[0-9]{3}[-]+[0-9]{3,4}[-]+[0-9]{4}$|^[0-9]{10,11}$'
+}
+
 def validate_email(email): 
-    email_regex = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
-    return re.search(email_regex, email)
+    return re.search(REGEX['email'], email)
 
 def validate_password(password):
-    password_regex = '[A-Za-z0-9@#$%^&+=]{8,}'
-    return re.search(password_regex, password)
+    return re.search(REGEX['password'], password)
 
 def validate_name(name):
-    name_regex = '''^([A-Z][a-z]+([ ]?[a-z]?['-]?[A-Z][a-z]+)*)$|^[가-힣]'''
-    return re.search(name_regex, name)
+    return re.search(REGEX['name'], name)
 
 def validate_phone_number(phone_number):
-    phone_number_regex = '^[0-9]{3}[-]+[0-9]{3,4}[-]+[0-9]{4}$|^[0-9]{10,11}$'
-    return re.search(phone_number_regex, phone_number)
+    return re.search(REGEX['phone_number'], phone_number)
--- students/junyoung-lee/user/views.py
+++ students/junyoung-lee/user/views.py
@@ -1,11 +1,16 @@
 import json, re
 
 from django.db.utils import IntegrityError
-from django.http import JsonResponse
-from django.views import View
+from django.http     import JsonResponse
+from django.views    import View
 
-from user.models import User
-from user.validators import validate_email, validate_password, validate_name, validate_phone_number
+from user.models     import User
+from user.validators import (
+    validate_email, 
+    validate_password, 
+    validate_name, 
+    validate_phone_number
+)
 
 class SignupView(View):
     def post(self, request):
@@ -13,12 +18,16 @@ class SignupView(View):
         try:
             if not validate_email(data['email']):
                 return JsonResponse({'MESSAGE':'INVALID_EMAIL'}, status = 400)
+            
             if not validate_password(data['password']):
                 return JsonResponse({'MESSAGE':'INVALID_PASSWORD'}, status = 400)
+            
             if not validate_name(data['name']):
                 return JsonResponse({'MESSAGE':'INVALID_NAME'}, status = 400)
+            
             if not validate_phone_number(data['phone_number']):
                 return JsonResponse({'MESSAGE':'INVALID_PHONE_NUMBER'}, status = 400)
+            
             data['phone_number'] = re.sub('\-', '', data['phone_number'])
             user = User.objects.create(
                 name         = data['name'],
@@ -28,7 +37,9 @@ class SignupView(View):
                 password     = data['password'],
             )
             return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+
         except IntegrityError:
             return JsonResponse({'MESSAGE':'USER_ALREADY_EXISTS'}, status=400)
