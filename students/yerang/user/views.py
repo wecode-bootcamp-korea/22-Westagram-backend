d11c09ae7242281f3f3607a25de271a51ef56eea
yerang <grondin0425@gmail.com>
2021-06-26 14:02:49 +0900

    Add:try_except

--- students/yerang/user/views.py
+++ students/yerang/user/views.py
@@ -9,30 +9,35 @@ from user.validation import email_validate, password_validate
 class SignUpView(View):
     def post(self, request):
         data = json.loads(request.body)
+        
+        try:
+            if not (data['email'] and data['password']):
+                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if not (data['email'] and data['password'] and data['phone_number'] and data['nickname']):
+            if not email_validate(data['email']):
+                return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+
+            if not password_validate(data['password']):
+                return JsonResponse({'message': 'PASSWORD_TOO_SHORT'}, status=400)
+
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
+            
+            if User.objects.filter(phone_number=data['phone_number']).exists():
+                return JsonResponse({'message': 'PHONENUMBER_ALREADY_EXISTS'}, status=400)
+            
+            if User.objects.filter(nickname=data['nickname']).exists():
+                return JsonResponse({'message': 'NICKNAME_ALREADY_EXISTS'}, status=400)
+            
+            User.objects.create(
+                email        = data['email'],
+                password     = data['password'],
+                phone_number = data['phone_number'],
+                nickname     = data['nickname'],
+            )
+        except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
-        if not email_validate(data['email']):
-            return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
-
-        if not password_validate(data['password']):
-            return JsonResponse({'message': 'PASSWORD_TOO_SHORT'}, status=400)
-
-        if User.objects.filter(email=data['email']).exists():
-            return JsonResponse({'message': 'EMAIL_ALREADY_EXISTS'}, status=400)
-        
-        if User.objects.filter(phone_number=data['phone_number']).exists():
-            return JsonResponse({'message': 'PHONENUMBER_ALREADY_EXISTS'}, status=400)
-        
-        if User.objects.filter(nickname=data['nickname']).exists():
-            return JsonResponse({'message': 'NICKNAME_ALREADY_EXISTS'}, status=400)
-        
-        User.objects.create(
-            email        = data['email'],
-            password     = data['password'],
-            phone_number = data['phone_number'],
-            nickname     = data['nickname'],
-        )
+        except Exception as e:
+            return JsonResponse({'message': e})
 
         return JsonResponse({'message': 'SUCCESS'}, status=201)
