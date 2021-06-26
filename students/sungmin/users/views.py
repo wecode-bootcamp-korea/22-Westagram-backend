c0d7a6fbec54b23536efc85849c5bee13117ed31
tjdlas2412 <alzm2412@gmail.com>
2021-06-26 13:35:30 +0900

    add:view.py

--- students/sungmin/users/views.py
+++ students/sungmin/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json, re
 
-# Create your views here.
+from django.views   import View
+from django.http    import JsonResponse
+
+from .models        import User
+
+
+class SignUpView(View):
+     def post(self, request):
+        data                 = json.loads(request.body)
+        email_regex          = re.compile(r'^[a-zA-Z0-9+-._]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+        password_regex       = re.compile(r'[a-zA-Z0-9`~!@#$%^&*()_+-={};:",./<>?]{8,25}')
+
+        try:
+            if not email_regex.match(data['email']):
+                return JsonResponse({"message":"Invald_email_format"}, status=400)
+            if not password_regex.match(data['password']):
+                return JsonResponse({"message":"Invalid_password"}, status=400)
+            if User.objects.filter(phone_number=data['phone_number']).exists():
+                return JsonResponse({"message":"Phonenumber_exist"}, status=400)
+            if User.objects.filter(nickname=data['nick_name']).exists():
+                return JsonResponse({"message":"nickname_exist"}, status=400)    
+            if User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"message":"email_exist"}, status=400)
+
+            User.objects.create(
+                email        = data['email'],
+                password     = data['password'],
+                phonenumber  = data['phonenumber'],
+                nick_name    = data['nick_name'],               
+                )
+            return JsonResponse({"message": "SUCCESS!"}, status=200)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
