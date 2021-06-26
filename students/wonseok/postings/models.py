0ef55464618381e3e91dd3cfe1c8fb7f484fedce
wonseok2877 <wonseok2877@gmail.com>
2021-06-26 20:12:40 +0900

    ADD : postings app

--- students/wonseok/postings/models.py
+++ students/wonseok/postings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+class Post(models.Model):
+    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_url   = models.URLField()
+    content     = models.TextField()
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    class Meta():
+        db_table = "posts"
