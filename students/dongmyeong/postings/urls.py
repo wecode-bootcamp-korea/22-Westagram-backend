e630c6a5665f8e2416bf8ac4a38adbdc249b1a8c
Dongmyeong Lee <dom9316@gmail.com>
2021-06-27 22:04:12 +0900

    Add: Update Feature for postings, comments

--- students/dongmyeong/postings/urls.py
+++ students/dongmyeong/postings/urls.py
@@ -4,7 +4,7 @@ from postings.views import LikeView, PostingView, CommentView
 
 urlpatterns = [
         path(''                                            , PostingView.as_view()),
-        path('/<int:posting_id>'                          , PostingView.as_view()),
+        path('/<int:posting_id>'                           , PostingView.as_view()),
         path('/<int:posting_id>/comments'                  , CommentView.as_view()),
         path('/<int:posting_id>/comments/<int:comment_id>' , CommentView.as_view()),
         path('/<int:posting_id>/likes'                     , LikeView.as_view()),
--- students/dongmyeong/postings/views.py
+++ students/dongmyeong/postings/views.py
@@ -41,25 +41,37 @@ class PostingView(View):
 
         return JsonResponse({"results": results}, status=200)
 
-    def delete(self, request, posting_id):
+    def put(self, request, posting_id):
         data = json.loads(request.body)
 
         try:
-            user = User.objects.get(id=data['user'])
-            Posting.objects.get(id=posting_id, user=user).delete()
+            posting = Posting.objects.get(id=posting_id, user_id=data['user'])
+
+            posting.image_url  = data['image_url']
+            posting.save()
 
             return JsonResponse({"message": "SUCCESS"}, status=200)
 
         except Posting.DoesNotExist:
             return JsonResponse({"message": "INVALID_POSTING"}, status=400)
         
-        except User.DoesNotExist:
-            return JsonResponse({"message": "INVALID_USER"}, status=400)
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+    def delete(self, request, posting_id):
+        data = json.loads(request.body)
+
+        try:
+            Posting.objects.get(id=posting_id, user_id=data['user']).delete()
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except Posting.DoesNotExist:
+            return JsonResponse({"message": "INVALID_POSTING"}, status=400)
         
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
         
-
 class CommentView(View):
     def post(self, request, posting_id):
         data = json.loads(request.body)
@@ -104,23 +116,31 @@ class CommentView(View):
         except Posting.DoesNotExist:
             return JsonResponse({"message": "INVALID_POSTING"}, status=400)
 
+    def put(self, request, posting_id, comment_id):
+        data = json.loads(request.body)
+        
+        try:
+            comment = Comment.objects.get(id=comment_id, user_id=data['user'])
+
+            comment.contents = data['contents']
+            comment.save()
+
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+        except Comment.DoesNotExist:
+            return JsonResponse({"message": "INVALID_COMMENT"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
     def delete(self, request, posting_id, comment_id):
         data = json.loads(request.body)
         
         try:
-            posting = Posting.objects.get(id=posting_id)
-            user    = User.objects.get(id=data['user'])
+            Comment.objects.get(id=comment_id, user_id=data['user']).delete()
 
-            posting.comment_set.get(id=comment_id, user=user).delete()
-            
             return JsonResponse({"message": "SUCCESS"}, status=200)
 
-        except Posting.DoesNotExist:
-            return JsonResponse({"message": "INVALID_POSTING"}, status=400)
-        
-        except User.DoesNotExist:
-            return JsonResponse({"message": "INVALID_USER"}, status=400)
-        
         except Comment.DoesNotExist:
             return JsonResponse({"message": "INVALID_COMMENT"}, status=400)
 
