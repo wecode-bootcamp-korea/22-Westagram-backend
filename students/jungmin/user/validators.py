701925b2bb4ffea2ec47d2839c68b9449f3c6e93
서정민 <sayho2021@gmail.com>
2021-06-28 15:04:14 +0900

    Revise: phone validation, delete comments

--- students/jungmin/user/validators.py
+++ students/jungmin/user/validators.py
@@ -1,29 +1,18 @@
 import re
 
 from django.core.exceptions import ValidationError
-# from django.core.validators import validate_email
 
-# # email validation (validate_email: 장고 모듈 함수)
-# def validate_email_django(email):
-#     try:
-#         validate_email(email)
-#     except ValidationError:
-#         raise ValidationError
-
-# email validation (정규표현식)
 def validate_email_regex(email):
     email_validation = re.compile('^[a-zA-Z0-9]+[a-zA-Z0-9+-_.]*@+[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
     if not email_validation.match(email):
-        raise ValidationError
+        raise ValidationError('ValidationError')
 
-# password validation
 def validate_password(password):
     password_validation = re.compile('[a-zA-Z0-9]{8,45}')
     if not password_validation.match(password):
-        raise ValidationError
+        raise ValidationError('ValidationError')
 
-# phone number input check (입력값 중간 '-' 혹은 '.' 제거하기)
-def check_phone_number(phone):
-    phone_checked = re.sub('[-.]', '', phone)
-    return phone_checked
-    
+def validate_phone(phone):
+    phone_validation = re.compile('[0-9]{11}')
+    if not phone_validation.match(phone):
+        raise ValidationError('ValidationError')
--- students/jungmin/user/views.py
+++ students/jungmin/user/views.py
@@ -6,38 +6,32 @@ from django.db              import IntegrityError
 from django.core.exceptions import ValidationError
 
 from user.models            import User
-from user.validators        import validate_email_regex, validate_password, check_phone_number
+from user.validators        import validate_email_regex, validate_password, validate_phone
 
 class SignUpView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
 
-            # email validation (정규표현식)
             validate_email_regex(data['email'])
 
-            # password validation
             validate_password(data['password'])
 
-            # phone input check (입력값 중간 '-' 혹은 '.' 제거하기)
-            phone_checked = check_phone_number(data['phone'])
+            validate_phone(data['phone'])
 
             user = User.objects.create(
                 email    = data['email'],
                 password = data['password'],
-                phone    = phone_checked,
+                phone    = data['phone'],
                 nickname = data['nickname']
             )
             return JsonResponse({'message': 'SUCCESS'}, status=201)
 
-        # 필수 항목이 비어있는지 체크
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        # 계정 정보가 이미 등록되어 있는지 체크
         except IntegrityError:
-            return JsonResponse({'message': 'IntegrityError: already in use'})
+            return JsonResponse({'message': 'IntegrityError: already in use'}, status=409)
 
-        # email, password validation 
         except ValidationError:
             return JsonResponse({'message': 'ValidationError'}, status=400)
