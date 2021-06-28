6e8c0f5e55005739e4ad5c96187b56384d2406af
Junyoung Lee <ambitiouskyle@gmail.com>
2021-06-28 16:50:15 +0900

    FIXED: DELETE SIGNUP

--- students/junyoung-lee/user/urls.py
+++ students/junyoung-lee/user/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from user.views import SignupView, SigninView
+from user.views import SigninView
 
 urlpatterns = [
-    path('/signup', SignupView.as_view()),
     path('/signin', SigninView.as_view()),
 ]
