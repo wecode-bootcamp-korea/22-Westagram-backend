244203a920ca0cbaf7c23d096f0c7f86ccf90c2f
Dongmyeong Lee <dom9316@gmail.com>
2021-06-28 18:58:05 +0900

    Fix: conflict

--- students/dongmyeong/users/views.py
+++ students/dongmyeong/users/views.py
@@ -13,37 +13,12 @@ class SigninView(View):
         data = json.loads(request.body)
 
         try:
-<<<<<<< HEAD
-            if not check_email_validation(data['email']) or \
-                    not check_password_validation(data['password']) or \
-                    not check_phone_validation(data['phone']):
-                        raise ValidationError("VALIDATION_ERROR")
-||||||| 844ea14
-            email    = check_email_validation(data['email'])
-            password = check_password_validation(data['password'])
-            phone    = check_phone_validation(data['phone'])
-            nickname = data['nickname']
-=======
-            if "email" in data:
-                user = User.objects.get(email=data['email'], password=data['password'])
-            elif "phone" in data:
-                user = User.objects.get(phone=data['phone'], password=data['password'])
-            else:
-                raise KeyError
->>>>>>> d60dd373289b8c266b150a1d081d723da391b2db
-
-<<<<<<< HEAD
-            User.objects.create(email=data['email'], password=data['password'], phone=data['phone'], nickname=data['nickname'])
-            return JsonResponse({"message": "SUCCESS"}, status=201)
-||||||| 844ea14
-            User.objects.create(email=email, password=password, phone=phone, nickname=nickname)
-            return JsonResponse({"message": "SUCCESS"}, status=201)
-=======
+            user = User.objects.get(email=data['email'], password=data['password'])
             return JsonResponse({"message": "SUCCESS"}, status=200)
->>>>>>> d60dd373289b8c266b150a1d081d723da391b2db
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=401)
 
         except User.DoesNotExist:
             return JsonResponse({"message": "INVALID_USER"}, status=401)
 
-        except KeyError:
-            return JsonResponse({"message": "KEY_ERROR"}, status=401)
