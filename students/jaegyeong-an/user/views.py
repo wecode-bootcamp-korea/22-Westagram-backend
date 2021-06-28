20096ec7a24c9312a4efeec23bbdfe3b1384bfa2
anjaekk <anjaekk@gmail.com>
2021-06-28 16:13:39 +0900

    Add: phone number로그인 기능 추가

--- students/jaegyeong-an/user/views.py
+++ students/jaegyeong-an/user/views.py
@@ -7,20 +7,21 @@ from django.db    import IntegrityError
 
 from user.models  import User
 
+REGEX = {
+    'email'    : '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+    'password' : '[A-Za-z0-9@#$%^&+=]{8,}'
+    }
+
 class SignUpView(View):
     def post(self, request):
-        REGEX = {
-            'email'    : '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
-            'password' : '[A-Za-z0-9@#$%^&+=]{8,}'
-        }
         try:
             data     = json.loads(request.body)
             email    = data['email']
             password = data['password']
 
-            if not REGEX['email'].match(email):
+            if not re.match(REGEX['email'], email):
                 return JsonResponse({'error':'INVALID_EMAIL'}, status=400)
-            if not REGEX['password'].match(password):
+            if not re.match(REGEX['password'], password):
                 return JsonResponse({'error':'INVALID_PASSWORD'}, status=400)
                 
             User.objects.create(
@@ -39,15 +40,21 @@ class SignUpView(View):
 
 class SignInView(View):
     def post(self, request):
-        data = json.loads(request.body)
         try:
-            email    = data['email']
+            data = json.loads(request.body)
+            account  = data['account']
             password = data['password']
 
-            if not User.objects.filter(email=email).exists():
-                return JsonResponse({'error': 'INVALID_USER'}, status=401)
-            if User.objects.get(email=email).password == password:
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+            if re.match(REGEX['email'], account):
+                if not User.objects.filter(email=account).exists():
+                    return JsonResponse({'error': 'INVALID_USER'}, status=401)
+                if User.objects.get(email=account).password == password:
+                    return JsonResponse({'message':'SUCCESS'}, status=200)
+            else:
+                if not User.objects.filter(phone_number=account).exists():
+                    return JsonResponse({'error': 'INVALID_USER'}, status=401)
+                if User.objects.get(phone_number=account).password == password:
+                    return JsonResponse({'message':'SUCCESS'}, status=200)
             return JsonResponse({'error': 'INVALID_USER'}, status=401)
 
         except KeyError:
