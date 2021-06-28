0b60b8091c48e2a6c2568c37af3409d084d42830
wonseok2877 <wonseok2877@gmail.com>
2021-06-28 22:42:12 +0900

    UPDATE : user.views.py bcrypt jwt기능구현

--- students/wonseok/user/views.py
+++ students/wonseok/user/views.py
@@ -42,3 +42,30 @@ class SignupView(View):
         except JSONDecodeError:
             return JsonResponse({"message": "JSON_DECODE_ERROR"}, status=400)
 
+class LoginView(View):
+    def post(self,request):
+        try:
+            data     = json.loads(request.body)
+            email    = data["email"]
+            password = data["password"]
+            if not validate_email(email):
+                raise ValidationError(message="INVALID_EMAIL")
+            if not validate_password(password):
+                raise ValidationError(message="INVALID_PASSWORD")
+            user     = User.objects.get(email=email)
+            encoded  = password.encode()
+            original = user.password.encode()
+            is_match = bcrypt.checkpw(encoded, original)
+            if not is_match:
+                raise ValidationError(message="INVALID_USER")
+            current_time = datetime.datetime.now()
+            five_hours = datetime.timedelta(hours=5)
+            exp = current_time + five_hours
+            access_token = jwt.encode({"id":user.id, "exp":exp}, my_settings.SECRET_KEY, my_settings.ALGORITHM)
+            return JsonResponse({"message":"success", "access_token":access_token}, status=200)
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+        except ValidationError as error:
+            return JsonResponse({"message":error.message}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"message":"USER_NOT_EXIST"}, status=400)
