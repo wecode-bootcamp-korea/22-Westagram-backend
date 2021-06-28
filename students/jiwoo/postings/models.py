21ee6cd86ce9445d6717936b58d9a3f0711092b3
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-29 01:29:18 +0900

    update : arragne&save

--- students/jiwoo/postings/models.py
+++ students/jiwoo/postings/models.py
@@ -3,22 +3,22 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model): 
-    user = models.ForeignKey('users.User',on_delete=models.CASCADE)
-    title = models.CharField(max_length= 100)
-    content = models.CharField(max_length = 1000)
-    image_url = models.URLField(max_length = 200)
+    user       = models.ForeignKey('users.User',on_delete=models.CASCADE)
+    title      = models.CharField(max_length= 100)
+    content    = models.CharField(max_length = 1000)
+    image_url  = models.URLField(max_length = 200)
     created_at = models.DateTimeField(auto_now = True)
     updated_at = models.DateTimeField(auto_now_add = True)
 
     class Meta: 
         db_table = 'posts'
 
-class Comment(models.Model):
-    user = models.ForeignKey('users.User',on_delete=models.CASCADE)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
-    content = models.CharField(max_length=1000)
+class Comment(models.Model): 
+    user       = models.ForeignKey('users.User',on_delete=models.CASCADE)
+    post       = models.ForeignKey('Post', on_delete=models.CASCADE)
+    content    = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
+    class Meta: 
         db_table = 'comments'
--- students/jiwoo/postings/views.py
+++ students/jiwoo/postings/views.py
@@ -7,48 +7,41 @@ from postings.models         import Post, Comment
 from users.models            import User
 
 
-class CreatePostsView(View):
-    def post(self, request):
-
+class CreatePostsView(View): 
+    def  post(self, request): 
         try: 
             data = json.loads(request.body)
-            user = User.objects.get(name=data['name'])            
+            user = User.objects.get(name=data['name'])
             Post.objects.create(
-                user=user,
-                title = data['title'],
-                content = data['content'],
+                user      = user,
+                title     = data['title'],
+                content   = data['content'],
                 image_url = data['image_url']
             )            
             return JsonResponse({"message":"SUCCESS"},status =201)
-
-        except KeyError:
+        except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
 
-
-    def get(self, request):
-        results= [({"user" : user.name, "post" : [(post.title, post.content, post.created_at) for post in user.post_set.all()]}) for user in User.objects.all()]       
-
+    def get(self, request): 
+        results = [({"user" : user.name, "post" : [(post.title, post.content, post.created_at) for post in user.post_set.all()]}) for user in User.objects.all()]
         return JsonResponse({"results":results}, status = 200)
 
-class CreateCommentsView(View):
-    def post(self, request):
+
+class CreateCommentsView(View): 
+    def post(self, request): 
         try: 
-            data=json.loads(request.body)
-            user=User.objects.get(name=data['name'])
-            post=Post.objects.get(title=data['title'])
+            data = json.loads(request.body)
+            user = User.objects.get(name=data['name'])
+            post = Post.objects.get(title=data['title'])
             Comment.objects.create(
-                user=user,
-                post=post,
-                content=data['content']
+                user    = user,
+                post    = post,
+                content = data['content']
             )
-            return JsonResponse({"message":"SUCCESS"}, status =201)
-        
-        except KeyError:
+            return JsonResponse({"message":"SUCCESS"}, status =201)        
+        except KeyError: 
             return JsonResponse({"message":"KEY_ERROR"},status=400)
 
-
-    def get(self, request):
-        
+    def get(self, request):         
         results = [({"post": post.title, "comment": [(comment.content, comment.created_at, comment.user.name) for comment in post.comment_set.all()]}) for post in Post.objects.all()]
-
         return JsonResponse({"results":results}, status=200)
