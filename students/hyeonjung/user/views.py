e7f598958bfb8f979736f92fbcdc6fb9132ba490
hyeonjung choi <waithjno@gmail.com>
2021-06-28 13:26:09 +0900

    수정:views.py

--- students/hyeonjung/user/views.py
+++ students/hyeonjung/user/views.py
@@ -11,15 +11,17 @@ class SignUpView(View):
         try:
             data = json.loads(request.body) 
             
-            regex_email    = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-            regex_password = re.compile(r'^[a-zA-Z0-9]{8,20}$')
+            REGEX_EMAIL        = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+            REGEX_PASSWORD     = re.compile(r'^[a-zA-Z0-9]{8,20}$')
+            REGEX_PHONE_NUMBER = re.compile(r'^[0-9]{10,20}')
 
-            if len(data['phone_number']) < 11:
+            if not REGEX_PHONE_NUMBER.match(data['phone_number']):
                 return JsonResponse({"message" : "Invalid_phone_number_foramt"} , status = 400)
-            if not regex_password.match(data['password']):
+            if not REGEX_PASSWORD.match(data['password']):
                 return JsonResponse({"message" : "Invalid_password_foramt"} , status = 400)
-            if not regex_email.match(data['email']):
+            if not REGEX_EMAIL.match(data['email']):
                 return JsonResponse({"message" : "Invald_email_format"} , status = 400) 
+            
             if User.objects.filter(phone_number=data['phone_number']).exists(): 
                 return JsonResponse({"message" : "phone_number_exist"} , status = 400) 
             if User.objects.filter(nickname=data['nickname']).exists():
