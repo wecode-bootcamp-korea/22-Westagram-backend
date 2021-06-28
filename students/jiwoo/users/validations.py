f5fce6f3e8fd75f9822a8462a3c914437368a3eb
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2021-06-29 01:39:54 +0900

    update : bcrypt&jwt

--- students/jiwoo/users/validations.py
+++ students/jiwoo/users/validations.py
@@ -1,9 +1,26 @@
-import re 
+# validations.py
 
-def password_check(password):
-    password_regex = re.compile('.{8,45}')
-    return password_regex.match(password)
+import re ,bcrypt, jwt
+
+from my_settings import SECRET_KEY
 
 def email_check(email): 
     email_regex = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
     return email_regex.match(email)
+
+def password_check(password): 
+    password_regex = re.compile('.{8,45}')
+    return password_regex.match(password)
+
+def create_bcrypt(password): 
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password
+
+def check_bcrypt(password, hashed_password): 
+    return bcrypt.checkpw(password.encode('utf-8'),hashed_password)
+
+
+def create_jwt(num): 
+    SECRET       = SECRET_KEY
+    access_token = jwt.encode({'id' : num}, SECRET, algorithm = 'HS256')
+    return access_token 
