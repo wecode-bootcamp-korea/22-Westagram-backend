4672392e7ffb371c57b1802200bbdec0384564b5
yerang <grondin0425@gmail.com>
2021-06-28 16:08:29 +0900

    add try_except

--- students/yerang/user/views.py
+++ students/yerang/user/views.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 
 from django.http  import JsonResponse
 from django.views import View
@@ -8,16 +9,26 @@ from user.models     import User
 class SignInView(View):
     def post(self, request):
         data     = json.loads(request.body)
-        email    = data['email']
-        password = data['password']
+       
+        try:
+            email    = data['email']
+            password = data['password']
+          
+            if not (email and password):
+                return JsonResponse({'message': 'KEY_ERROR'}, status=400)
 
-        if not (email and password):
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            if User.objects.get(email=email).password != password:
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+        except ValueError:
+            return JsonResponse({'message': 'VALUE_ERROR'}, status=400)
+        except JSONDecodeError:
+            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
+        except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
-
-        if not User.objects.filter(email=email).exists():
-            return JsonResponse({'message': 'INVALID_USER'}, status=401)
-
-        if User.objects.get(email=email).password != password:
-            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+        except Exception as e:
+            return JsonResponse({'message': e})
 
         return JsonResponse({'message': 'SUCCESS'}, status=200)
