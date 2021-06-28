0c804decb56cd519c1d1d42c8363a9a4dcee4bdb
wlgns410 <wlgns410@gmail.com>
2021-06-28 10:03:56 +0900

    signup modify

--- students/jihoon/user/urls.py
+++ students/jihoon/user/urls.py
@@ -1,5 +1,5 @@
-from django.urls    import path
-from .views         import SignUpView
+from django.urls        import path
+from user.views         import SignUpView
 
 urlpatterns =[
     path('/signup',SignUpView.as_view()),
--- students/jihoon/user/views.py
+++ students/jihoon/user/views.py
@@ -1,45 +1,53 @@
 import json, re
 
-from json.decoder import JSONDecodeError
 from django.views import View
-from django.http  import JsonResponse
+from django.http  import JsonResponse, request
 
 from user.models import Account
 
-
 class SignUpView(View):
     def post(self, request):
-        data = json.loads(request.body)
-
-        email_regas     = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-        password_regex  = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
-        phone_number_regex = "\d{2}-\d{3}-\d{4}"
-
-        if data['name']     == '':
-            return JsonResponse({"MESSAGE": "필수 입력 사항입니다."})
-
-        if (re.search(phone_number_regex, data['phone_number']) != None) == False:
-            return JsonResponse({'MESSAGE':'필수 입력 사항입니다.'})
-
-        if (re.search(email_regas, data['email']) != None) == False:
-            return JsonResponse({"MESSAGE": "EMAIL VALIDATION"})
-
-        if (re.search(password_regex, data['password']) != None) == False:
-            return JsonResponse({'MESSAGE':'PASSWORD VALIDATION'})
-
-        if len(data['password']) < 8:
-            return JsonResponse({'MESSAGE':'PASSWORD ERROR'})
-
         try:
+            data                = json.loads(request.body)    
+            NAME_REGEX          = "^[가-힣]{2,4}$"
+            NICKNAME_REGEX      = "^[a-zA-Z0-9._-]{2,16}\$"
+            PHONE_NUMBER_REGEX  = "^\d{3}-\d{3,4}-\d{4}$"
+            EMAIL_REGEX         = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            PASSWORD_REGEX      = "^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$"
+            
+
+            if re.search(NAME_REGEX, data['name'])                   == False:
+                return JsonResponse({"MESSAGE": "2~4 글자의 한글을 입력해 주세요."}, status=400)
+            
+            if re.search(NICKNAME_REGEX, data['nickname'])           == False:
+                return JsonResponse({"MESSAGE" : "숫자, 영어, 언더스코어만 사용할 수 있습니다."}, status=400)
+
+            if re.search(PHONE_NUMBER_REGEX, data['phone_number'])   == False:
+                return JsonResponse({"MESSAGE":"필수 입력 사항입니다."}, status=400)
+
+            if re.search(EMAIL_REGEX, data['email'])                 == False:
+                return JsonResponse({"MESSAGE": "EMAIL VALIDATION"}, status=400)
+
+            if re.search(PASSWORD_REGEX, data['password'])           == False:
+                return JsonResponse({"MESSAGE":"PASSWORD VALIDATION"}, status=400)
+
+            if Account.objects.filter(email=data['email']).exists():
+                return JsonResponse({"MESSAGE": "이미 존재하는 이메일입니다."}, status=400)
+
+            if Account.objects.filter(phone_number=data['phone_number']).exists():
+                return JsonResponse({"MESSAGE": "이미 존재하는 번호입니다."}, status=400)
+
+            if Account.objects.filter(nickname=data['nickname']).exists():
+                return JsonResponse({"MESSAGE": "이미 존재하는 닉네임입니다."}, status=400)
+            
             Account.objects.create(
-                name = data['name'],
-                email = data['email'],
-                phone_number = data['phone_number'],
-                password = data['password'],
-                nickname = data['nickname']
+                name            = data['name'],
+                email           = data['email'],
+                phone_number    = data['phone_number'],
+                password        = data['password'],
+                nickname        = data['nickname']
             )
-            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+            return JsonResponse({"MESSAGE":"SUCCESS"}, status=201)
 
         except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
-
+            return JsonResponse({"MESSAGE":"KEY_ERROR"}, status=400)
