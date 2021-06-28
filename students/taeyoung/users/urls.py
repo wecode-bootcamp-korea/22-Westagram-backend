ea0fac6c2361bec3d4abc97477f86bc4809ade5b
ty <kim@tyui-MacBookAir.local>
2021-06-28 17:43:54 +0900

    re

--- students/taeyoung/users/urls.py
+++ students/taeyoung/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import AccountView
  
 urlpatterns = [
-    path('/signup', AccountView.as_view())
+    path('/signup', AccountView.as_view()),
+    path('/signin', AccountView.as_view())
 ]
--- students/taeyoung/users/views.py
+++ students/taeyoung/users/views.py
@@ -5,36 +5,6 @@ from django.http    import JsonResponse
 
 from users.models   import Account
 
-class SignInView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-
-    #      try:
-    #          if not (data['email'] and data['password']):
-    #              return JsonResponse({'message':'KEY_ERROR'}, status=401)
-    #          if Account.objects.filter(email=data['email']).exists():
-    #              email = Account.objects.get(email=data['email'])
-    #              if email.password == data['password']:
-    #                  return JsonResponse({'message': 'SUCCESS'}, status=200)
-    #              else:
-    #                  return JsonResponse({'message': 'INAVLID_PASSWORD'}, status=401)
-    #          else:
-    #              return JsonResponse({'message': 'INVALID_USER'}, status=401)
-    #      except KeyError:
-            #  return JsonResponse({'message': 'KEY_ERROR'}, status=401)
-
-        try:
-            if Account.objects.filter(email=data['email']).exists():
-                email = Account.objects.get(email=data['email'])
-                if email.password == data['password']:
-                    return JsonResponse({'message': 'SUCCESS'}, status=200)
-                else:
-                    return JsonResponse({'message':'INVALID_PASSWORD'}, status=401)
-            else:
-                return JsonResponse({'message':'INAVLID_USER'}, status=401)
-        except KeyError:
-            return JsonResponse({'message': 'KeyError'}, status=400)
-
 class AccountView(View):
     def post(self, request):
         try:
@@ -67,3 +37,19 @@ class AccountView(View):
             return JsonResponse({'message': 'SUCCESS'}, status=201)
         except KeyError:
             return JsonResponse({'meesage': 'KEY_ERROR'}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            if Account.objects.filter(email=data['email']).exists():
+                email = Account.objects.get(email=data['email'])
+                if email.password == data['password']:
+                    return JsonResponse({'message': 'SUCCESS'}, status=200)
+                else:
+                    return JsonResponse({'message':'INVALID_PASSWORD'}, status=401)
+            else:
+                return JsonResponse({'message':'INAVLID_USER'}, status=401)
+        except KeyError:
+            return JsonResponse({'message': 'KEYERROR'}, status=400)
