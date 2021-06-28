bcd88ffa03b025004187020220c85fc2d244c863
ty <kim@tyui-MacBookAir.local>
2021-06-28 13:30:54 +0900

    modify signup

--- students/taeyoung/users/views.py
+++ students/taeyoung/users/views.py
@@ -11,19 +11,22 @@ class AccountView(View):
             data            = json.loads(request.body)
             email_regexr    = re.compile(r'^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
             phone_regexr    = re.compile(r'^\d{3}-\d{3,4}-\d{4}')
-            
+            password_regexr = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$')
+
+
             if Account.objects.filter(email=data['email']).exists():
                 return JsonResponse({'message': '이미 존재하는 이메일입니다.'}, status=400)
-            if not email_regexr.match(data['email']):
-                return JsonResponse({'message': '잘못된 형식의 이메일입니다.'}, status=400)
-            if len(data['password']) < 8:
-                return JsonResponse({'message': '비밀번호는 8자리 이상이어야만 합니다.'}, status=400)
             if Account.objects.filter(nickname=data['nickname']).exists():
                 return JsonResponse({'message': '이미 존재하는 닉네임입니다.'}, status=400)
             if Account.objects.filter(phone_number=data['phone_number']).exists():
                 return JsonResponse({'message': '이미 존재하는 번호입니다.'}, status=400)
             if not phone_regexr.match(data['phone_number']):
                 return JsonResponse({'message': '잘못된 형식의 번호입니다.'}, status=400)
+            if not email_regexr.match(data['email']):
+                return JsonResponse({'message': '잘못된 형식의 이메일입니다.'}, status=400)
+            if not password_regexr.match(data['password']): 
+                return JsonResponse({'message': '잘못된 형식의 비밀번호입니다.'}, status=400)
+
 
             Account.objects.create(
                 name          = data['name'],
@@ -34,5 +37,5 @@ class AccountView(View):
             )
         
             return JsonResponse({'message': 'SUCCESS'}, status=201)
-        except KEYERROR:
+        except KeyError:
             return JsonResponse({'meesage': 'KEY_ERROR'}, status=400)
