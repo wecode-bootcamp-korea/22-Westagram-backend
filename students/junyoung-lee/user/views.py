0c8aa2dfb5822692ce33b20daa72104f49cafd66
Junyoung Lee <ambitiouskyle@gmail.com>
2021-06-29 14:03:02 +0900

    ADD: User signin auth

--- students/junyoung-lee/user/views.py
+++ students/junyoung-lee/user/views.py
@@ -1,9 +1,11 @@
-import json, bcrypt
+import json, jwt, bcrypt
 
+from django.core.exceptions import MultipleObjectsReturned
 from django.db.utils        import IntegrityError
 from django.http            import JsonResponse
 from django.views           import View
 
+from my_settings     import SECRET_KEY
 from user.models     import User
 from user.validators import (
     validate_email, 
@@ -43,3 +45,26 @@ class SignupView(View):
 
         except IntegrityError:
             return JsonResponse({'MESSAGE':'USER_ALREADY_EXISTS'}, status=400)
+
+class SigninView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            user = User.objects.get(email=data['email'])
+            if bcrypt.checkpw(
+                data['password'].encode('utf-8'), 
+                user.password.encode('utf-8')
+                ):
+                encoded_jwt = jwt.encode({'user_id':user.pk}, SECRET_KEY, algorithm='HS256')
+                return JsonResponse({'MESSAGE':'SUCCESS', 'ACCESS_TOKEN':encoded_jwt}, status=200)
+
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        except MultipleObjectsReturned:
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
