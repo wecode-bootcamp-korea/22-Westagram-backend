5a1e7cc6cdb7950639f6551b64342632bdb53a62
yerang <grondin0425@gmail.com>
2021-06-29 15:07:12 +0900

    add: 패스워드 암호화, 토큰 발행

--- students/yerang/user/urls.py
+++ students/yerang/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import SignInView
+from user.views import SignInView, SignUpView
 
 urlpatterns = [
+    path('/signup', SignUpView.as_view()),
     path('/signin', SignInView.as_view()),
 ]
