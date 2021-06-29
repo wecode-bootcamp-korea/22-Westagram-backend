ddb7437cba933e113b99e33f5e9bf31cb8d62f2a
Jay9393 <tlswo1313@gmail.com>
2021-06-29 11:33:45 +0900

    asd

--- students/sinjae/users/views.py
+++ students/sinjae/users/views.py
@@ -7,45 +7,54 @@ from django.views                 import View
 
 from users.models                 import User
 
-class UserSignUp(View):
+
+class SignUp(View):
     def post(self, request):
-        data = json.loads(request.body)
-        password            = data["password"]
-        email               = data["email"]
-        phone_number        = data["phone_number"]
-        users               = User.objects.all()
-        users_email_data    = [user.email for user in users]
-        users_password_data = [user.password for user in users]
+        try:
+            data                = json.loads(request.body)
+            password            = data["password"]
+            email               = data["email"]
+            phone_number        = data["phone_number"]
+            duplicate_user      = User.objects.filter(email=data["email"]).exists() or User.objects.filter(phone_number=data["phone_number"]).exists()
 
-        # email or password 가 전달 되지 않았을 경우
-        if email is False or password is False:
-            return JsonResponse({"message": "KEY_ERROR"}, status=400 )
-        
-        # eamil validation
-        elif re.match('^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$', email) is None:
-            return JsonResponse({"message": "INAPPROPRIATE_EMAIL"}, status=400 )
-        
-        # password validation
-        elif re.match('[a-zA-Z0-9\!\@\#\$\?]{8,}', password) is None:            
-            return JsonResponse({"message": "INAPPROPRIATE_PASSWORD"}, status=400 )
+            if not email_validation(email):
+                return JsonResponse({"message": "INAPPROPRIATE_EMAIL"}, status=400 )
+            if not password_validation(password):
+                return JsonResponse({"message": "INAPPROPRIATE_PASSWORD"}, status=400 )
+            if not phone_validation(phone_number):
+                return JsonResponse({"message": "INAPPROPRIATE_PHONE_NUMBER"}, status=400 )
+            if duplicate_user:
+                return JsonResponse({"message": "ALREADY_USER"}, status=400 )
 
-        # phone_number validation
-        elif re.match('[0-9]{10,12}', phone_number) is None:
-            return JsonResponse({"message": "INAPPROPRIATE_PHONE_NUMBER"}, status=400 )
-            
-        # 중복 가입
-        elif email in users_email_data or phone_number in users_password_data:
-            return JsonResponse({"message": "ALREADY_USER"}, status=400 )
-
-        else:
             User.objects.create(
-            nick_name       = data["nick_name"],
-            name            = data["name"],
-            password        = data["password"],
-            email           = data["email"],
-            phone_number    = data["phone_number"],
-            gender          = data["gender"],
-            birth_date      = data["birth_date"],
-        )
+                nick_name       = data["nick_name"],
+                name            = data["name"],
+                password        = data["password"],
+                email           = data["email"],
+                phone_number    = data["phone_number"],
+                gender          = data["gender"],
+                birth_date      = data["birth_date"],
+            )
 
-            return JsonResponse({"MESSAGE": "SUCESS"}, status=201)
+            return JsonResponse({"message": "SUCCESS"}, status=201)
+
+        except KeyError:
+            return JsonResponse({"message": "KEYERROR"}, status=400 )
+
+class SignIn(View):
+    def post(self, request):
+        try:
+            data          = json.loads(request.body)
+            email         = data["email"]
+            password      = data["password"]
+            if User.objects.filter(email=email).exists() :
+                user = User.objects.get(email=email)
+                if password == user.password:
+                    access_token = jwt.encode({"user": user.id}, SECRET_KEY, algorithm = 'HS256')
+                    return JsonResponse({"token": access_token}, status=200)
+
+            else:
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
