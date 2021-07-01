3552794ac8234b7d59152e96de0c7ff3f3d64527
서정민 <sayho2021@gmail.com>
2021-07-01 21:16:33 +0900

    Add: Update posts and comments

--- students/jungmin/postings/views.py
+++ students/jungmin/postings/views.py
@@ -2,6 +2,7 @@ import json
 from django.http.response import JsonResponse
 
 from django.views import View
+from django.utils import timezone
 
 from user.models import User
 from postings.models import Posting, Comment, Like
@@ -20,8 +21,6 @@ class PostingView(View):
         postings = Posting.objects.all()
         results = []
 
-
-        
         for posting in postings:
             results.append(
                 {
@@ -32,12 +31,21 @@ class PostingView(View):
                     'text': posting.text
                 }
             )
+
         return JsonResponse({'results': results}, status=200)
 
     def delete(self, request, post_id):
         Posting.objects.filter(id=post_id).delete()
 
-        return JsonResponse({'message': 'DELETED'})
+        return JsonResponse({'message': 'DELETED'}, status=200)
+
+    def put(self, request, post_id):
+        data = json.loads(request.body)
+        post = Posting.objects.filter(id=post_id)
+        updated_at = timezone.now()
+        post.update(image_url=data['image_url'], text=data['text'], updated_at=updated_at)
+
+        return JsonResponse({'message': 'UPDATED'}, status=200)
 
 class CommentView(View):
     def post(self, request):
@@ -61,10 +69,7 @@ class CommentView(View):
                 {
                     'post': post.id,
                     'user': post.user.id,
-                    'comments': [
-                        {'user': comment.user.id, 'text': comment.text} 
-                        for comment in comments
-                    ]
+                    'comments': [{'user': comment.user.id, 'text': comment.text} for comment in comments]
                 }
             )
         return JsonResponse({'results': results}, status=200)
@@ -72,16 +77,29 @@ class CommentView(View):
     def delete(self, request, comment_id):
         Comment.objects.filter(id=comment_id).delete()
 
-        return JsonResponse({'message': 'DELETED'})
+        return JsonResponse({'message': 'DELETED'}, status=200)
+
+    def put(self, request, comment_id):
+        data = json.loads(request.body)
+        post = Posting.objects.filter(id=comment_id)
+        post.update(text=data['text'])
+
+        return JsonResponse({'message': 'UPDATED'}, status=200)
         
 class LikeView(View):
     def post(self, request):
         data = json.loads(request.body)
-
-        Like.objects.create(
-            post = Posting.objects.get(id=data['post']),
-            user = User.objects.get(id=data['user'])
-        )
+        
+        # Like
+        if not Like.objects.filter(post=data['post'], user=data['user']).exists():
+            Like.objects.create(
+                post = Posting.objects.get(id=data['post']),
+                user = User.objects.get(id=data['user'])
+            )
+        
+        #UnLike
+        else:
+            Like.objects.filter(post=data['post'], user=data['user']).delete()
 
         return JsonResponse({'message': 'SUCCESS'}, status=201)
 
@@ -98,3 +116,4 @@ class LikeView(View):
                 }
             )
         return JsonResponse({'results': results}, status=200)
+
